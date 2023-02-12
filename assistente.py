b727d750b3331d6bb9e6659b8bc562c60480f0c5
Mateus-IC1101 <mateus.dolglas6@gmail.com>
2023-02-12 15:49:21 -0300

    [Speech | Api google | e etc]
    Teste antes da sepração por funções para implementação da assistente

--- assistente.py
+++ assistente.py
@@ -1,33 +1,49 @@
 import data.respostas as __REPOSITORY_RESPOSTAS__
 import pyttsx3
 import helpers.datetimeHelper as datetimeHelper
+from gtts import gTTS
+import subprocess as s
 import webbrowser
-import tensorflow as tf
-engine = pyttsx3.init()
-rate = engine.getProperty('rate')
-voices = engine.getProperty('voices')
-engine.setProperty("voice", voices[59].id)
-engine.setProperty('rate', rate - 70)
-volume = engine.getProperty('volume')
-engine.setProperty('volume', volume - 0.1)
-engine.say('Você não tem eventos para hoje')
+# import tensorflow as tf
+# Teste
+# print(__REPOSITORY_RESPOSTAS__.EMOCOES)
+#
+# ----------------------------------- voz api google-----------------------------
 
+voz = gTTS('Você não tem eventos para hoje', lang="pt")
+voz.save('voz.mp3')
+s.call(['mplayer', 'voz.mp3'])
+
+# ----------------------------------- voz api google-----------------------------
+
+# ----------------------------------- voz pyttsx3 -----------------------------
+
+engine = pyttsx3.init()
+rate = engine.getProperty('rate')  # palavras por min
+engine.setProperty('rate', 100)  # ajuste de palavras por min
+voices = engine.getProperty('voices')  # vozes
+volume = engine.getProperty('volume')  # volume atual
+engine.setProperty('volume', volume - 0.1)  # ajuste de volume
+# atribuindo índice de voz brasileira
+engine.setProperty("voice", voices[59].id)
+engine.say('Você não tem eventos para hoje')  # entrada de texto
 engine.runAndWait()
 engine.stop()
+# ----------------------------------- voz pyttsx3 -----------------------------
 
+
+# ----------------------------------- webbrowser -----------------------------
 path = 'xdg-open google.com'
-webbrowser.open('https://www.python.org')
+pesquisa = 'python'
+webbrowser.open('https://www.google.com.br/search?q='+pesquisa)
 
 
-print(__REPOSITORY_RESPOSTAS__.EMOCOES)
+# def loadModel(): # desativado temporariamente
+#     model = tf.keras.models.load_model(
+#         'models/speech_emotion_recognition.hdf5')
+#     model_dict = sorted(list(['Feliz', 'Triste', 'Cansado']))
+#     TAXA = 48000
+#     return model, model_dict, TAXA
 
 
-def loadModel():
-    model = tf.keras.models.load_model(
-        'models/speech_emotion_recognition.hdf5')
-    model_dict = sorted(list(['Feliz', 'Triste', 'Cansado']))
-    TAXA = 48000
-    return model, model_dict, TAXA
-
-
-print(loadModel()[0].summary())
+# print(loadModel()[0].summary())
